82407e155679a242c6df6e921e5437c89596c58a
juchas019 <juchniewiczlukasz019@gmail.com>
2018-07-18 21:57:55 +0200

    FZ9CB-12-add function anonymys change var/let

--- js/animation_about-us.js
+++ js/animation_about-us.js
@@ -1,21 +1,24 @@
-var $animation_elements = $('.about-us__team-members');
-var $window = $(window);
+$(function () {
+    let $animation_elements = $('.about-us__team-members');
+    let $window = $(window);
 
-function check_if_in_view() {
-    var window_height = $window.height();
-    var window_top_position = $window.scrollTop();
-    var window_bottom_position = (window_top_position + window_height);
+    function check_if_in_view() {
+        let window_height = $window.height();
+        let window_top_position = $window.scrollTop();
+        let window_bottom_position = (window_top_position + window_height);
 
-    $.each($animation_elements, function() {
-        var $element = $(this);
-        var element_height = $element.outerHeight();
-        var element_top_position = $element.offset().top;
-        var element_bottom_position = (element_top_position + element_height);
+        $.each($animation_elements, function() {
+            let $element = $(this);
+            let element_height = $element.outerHeight();
+            let element_top_position = $element.offset().top;
+            let element_bottom_position = (element_top_position + element_height);
 
-        if ((element_bottom_position >= window_top_position) &&
-            (element_top_position <= window_bottom_position)) {
-            $element.addClass('in-view');
-        }
-    });
-}
-$window.on('scroll resize', check_if_in_view);
+            if ((element_bottom_position >= window_top_position) &&
+                (element_top_position <= window_bottom_position)) {
+                $element.addClass('in-view');
+            }
+        });
+    }
+    $window.on('scroll resize', check_if_in_view);
+    $window.trigger('scroll');
+})
