efd4a0883ffab1406615cf5a95f614d9eecb82d0
martapienkowska <marta.pienkowska@gmail.com>
2018-07-29 19:15:26 +0200

    back to top upgrated

--- js/back-to-top.js
+++ js/back-to-top.js
@@ -1,7 +1,20 @@
-$('.go-top').click(function() {
-    $('html, body').animate({
-        scrollTop: 0
-    }, 900);
-    return false;
-});
 
+$(function(){
+    var stt_is_shown = false;
+    $(window).scroll(function(){
+
+        var win_height = 300;
+        var scroll_top = $(document).scrollTop();
+        if (scroll_top > win_height && !stt_is_shown) {
+            stt_is_shown = true;
+            $("#scroll-to-top").fadeIn();
+        } else if (scroll_top < win_height && stt_is_shown) {
+            stt_is_shown = false;
+            $("#scroll-to-top").fadeOut();
+        }
+    });
+    $("#scroll-to-top").click(function(e){
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, 1500);
+    });
+});
